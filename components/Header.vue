402c1a92284823f8518ed5f0fe2aac3f95a4d968
yashweblife <yashweblife20@gmail.com>
2022-12-04 17:46:52 -0600

    added basic stuff

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>Workflow Manager</h1>
+        <nav>
+            <NuxtLink to="settings">
+                Settings
+            </NuxtLink>
+        </nav>
+    </header>
+</template>
+	
+<script>
+export default {
+	
+}
+</script>
+	
+<style scoped>
+	
+</style>
