f3ac447cf0f4172ec5591838c6fac5a7a2f989da
yashweblife <yashweblife20@gmail.com>
2022-12-04 18:11:15 -0600

    changed name

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>Workflow Manager</h1>
+        <nav>
+            <NuxtLink to="/settings">
+                Settings
+            </NuxtLink>
+            <NuxtLink to="/projects">
+                Projects
+            </NuxtLink>
+        </nav>
+    </header>
+</template>
+	
+<script>
+export default {
+    name:"HeaderComponent",
+    data(){
+        return{
+            
+        }
+    },
+}
+</script>
+	
+<style scoped>
+	
+</style>
